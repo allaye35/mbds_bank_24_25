a8c83aa4f1fa119a9f383b359dd975615228e9c9
Allaye Bocoum <allaye35@gmail.com>
2025-03-14 02:24:08 +0100

    eureka et discovery ajouter mais ya erreur

--- ms_bank_mbds_24_25/Account/src/main/java/fr/mbds/bank/account/CustomerClient.java
+++ ms_bank_mbds_24_25/Account/src/main/java/fr/mbds/bank/account/CustomerClient.java
@@ -1,5 +1,6 @@
 package fr.mbds.bank.account;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +8,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "customer", url = "http://localhost:8080")
+@FeignClient(name = "CUSTOMER")
 public interface CustomerClient {
 
 
 
 
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getAllCustomers")
     public List<Customer> getCustomers();
 
     @GetMapping("/customer/{id}")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getDefaultCustomer")
     public Customer getCustomer(@PathVariable("id") Long id);
+
+
+    default Customer getDefaultCustomer(Long id, Exception exception)
+    {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName("Not available");
+        customer.setLastName("Not available");
+        customer.setEmail("Not available");
+        return customer;
+    }
+    default List<Customer> getAllCustomers(Exception exception)
+    {
+        return List.of();
+    }
+
 }
